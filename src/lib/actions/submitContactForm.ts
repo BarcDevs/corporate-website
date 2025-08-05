86565542942941d24e03aac0ee0973a7f6d7f3da
Bar Cohen <94045477+BarcDevs@users.noreply.github.com>
2025-08-05 13:38:28 +0300

    feat(db) add api action to handle contact form

---
+++ src/lib/actions/submitContactForm.ts
@@ -0,0 +1,24 @@
+import axios from 'axios'
+import { ContactFormData } from '@/types/contactFormData'
+
+export const submitContactForm = async (formData: ContactFormData) => {
+    try {
+        const res = await
+            axios.post('/api/contact', formData)
+
+        return {
+            success: true,
+            data: res.data
+        }
+    } catch ( err: any ) {
+        const errorMessage =
+            err?.response?.data?.error ||
+            err.message ||
+            'Unexpected error'
+
+        return {
+            success: false,
+            error: errorMessage
+        }
+    }
+}
