ba3ebf9ee86dcb3ca9a58374c0f0f91ee621df51
Bar Cohen <94045477+BarcDevs@users.noreply.github.com>
2025-08-07 17:37:49 +0300

    rfc(footer) switch social media names to logos

--- src/data/footer.ts
+++ src/data/footer.ts
@@ -10,8 +10,9 @@ export const footerData: FooterLinks = {
     businessName: 'RenovationPro Services',
     address: '123 Main Street, Your City, State 12345',
     socialLinks: [
-        { platform: 'Facebook', url: '#' },
-        { platform: 'Instagram', url: '#' },
-        { platform: 'LinkedIn', url: '#' }
+        { platform: 'Facebook', url: 'https://www.facebook.com/' },
+        { platform: 'Instagram', url: 'https://www.instagram.com/' },
+        { platform: 'X', url: 'https://www.x.com/' },
+        { platform: 'LinkedIn', url: 'https://www.linkedin.com/feed/' }
     ]
 }
